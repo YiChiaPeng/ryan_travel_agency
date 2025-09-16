380bf7df0188d0b7e79c84606e969d4aece46702
chia <kent7493@icloud.com>
2025-09-16 11:00:21 +0800

    TEST

--- b2b-doc-management/db/init.sql
+++ b2b-doc-management/db/init.sql
@@ -1,8 +1,9 @@
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    company_name VARCHAR(255) NOT NULL,
+    company_name VARCHAR(255) NOT NULL UNIQUE,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    role ENUM('user', 'admin') NOT NULL DEFAULT 'user',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
